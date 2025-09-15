e39c01c0ac576ab0fabbb8874a4587daf483cd13
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-09-15 17:50:08 -0500

    prep Point class

---
+++ P04/full_credit/Point.java
@@ -0,0 +1,32 @@
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+
+    }
+    
+    public Point() {
+        this(0,0);
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+    @Override
+    public boolean equals(Object o) {
+
+    }
+    @Override
+    public int hashCode() {
+
+    }
+    @Override
+    public String toString() {
+        
+    }
+}
