1cbdec9dbea452bab962daf289fa7af7e040903c
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-09-15 17:53:43 -0500

    prep Polygon class

---
+++ P04/full_credit/Polygon.java
@@ -0,0 +1,20 @@
+public class Polygon {
+    public final int MAX_SIDES;
+    private int numSides;
+    private Point[] points;
+
+    public void addPoint(Point point) {
+
+    }
+
+    public double perimeter() {
+
+    }
+    @Override
+    public String toString() {
+
+    }
+    private double lineLength(Point p1, Point p2) {
+        
+    }
+}
