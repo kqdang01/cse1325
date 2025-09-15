6ace7ee20413c374805bb5ae66981a1368a7c367
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-09-15 18:19:37 -0500

    finish Polygon class implementation

--- P04/full_credit/Polygon.java
+++ P04/full_credit/Polygon.java
@@ -1,20 +1,36 @@
 public class Polygon {
-    public final int MAX_SIDES;
+    public static final int MAX_SIDES = 12;
     private int numSides;
-    private Point[] points;
+    private Point[] points = new Point[MAX_SIDES];
 
     public void addPoint(Point point) {
-
+        for (Point p : this.points) {
+            if (p.equals(point)) throw new IllegalArgumentException("Duplicate Point: " + point);
+        }
+        if (this.points.length == this.MAX_SIDES) throw new RuntimeException("Polygon is full");
+        this.points[numSides] = point;
+        this.numSides++;
     }
 
     public double perimeter() {
-
+        if (this.numSides < 3) throw new RuntimeException("Polygons required 3+ sides!");
+        int j = 0;
+        double sum = 0;
+        for (int i = 1; i < this.numSides-1; i++) {
+            sum += this.lineLength(points[j], points[i]);
+        }
+        sum += this.lineLength(points[0], points[numSides-1]);
+        return sum;
     }
     @Override
     public String toString() {
-
+        String pointString;
+        for (Point point : this.points) pointString += point + ",";
+        return "Polygon[" + pointString + "]";
     }
-    private double lineLength(Point p1, Point p2) {
-        
+    private static double lineLength(Point p1, Point p2) {
+        double x = Math.abs(p1.getX() - p2.getX());
+        double y = Math.abs(p1.getY() - p2.getY());
+        return Math.sqrt(x**2 + y**2);
     }
 }
