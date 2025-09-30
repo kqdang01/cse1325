2258b9b237f79d61328808c9f2ccdc7dfcf8f3cb
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-09-29 21:33:36 -0500

    Finish Rating.java

---
+++ P05/src/Rating.java
@@ -0,0 +1,23 @@
+public class Rating {
+    private int stars;
+    private Comment review;
+    
+    public Rating(int stars; Comment review) {
+        if (!(stars >= 1 && stars <= 5)) throw new IllegalArgumentException("Invalid stars.");
+        this.stars = stars;
+        this.review = review;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public Comment getReview() {
+        return review;
+    }
+
+    @Override
+    public String toString() {
+        return "\u2605".repeat(getStars()) + "\u2606".repeat(5-getStars());
+    }
+}
