b4759296aba90b7b3c34ea3fe426e103909b4976
kqdang01 <113054344+kqdang01@users.noreply.github.com>
2025-09-29 21:32:47 -0500

    Finish Comment.java

---
+++ P05/src/Comment.java
@@ -0,0 +1,45 @@
+public class Comment {
+    private String text;
+    private Person author;
+    private Comment inReplyTo;
+    private ArrayList<Comment> replies;
+
+    public Comment(String text; Person author; Comment inReplyTo) {
+        if (text.equals("") || text == null || author == null) throw new IllegalArgumentException("Invalid text or author.");
+        this.text = text;
+        this.author = author;
+        this.inReplyTo = inReplyTo;
+        this.replies = new ArrayList<>();
+    }
+
+    public void addReply(String text; Person author) {
+        if (text.equals("") || text == null || author == null) throw new IllegalArgumentException("Invalid text or author.");
+        Comment comment = new Comment(text, author, this);
+        replies.add(comment);
+    }
+
+    public int numReplies() {
+        return replies.size();
+    }
+
+    public Comment getReply(int index) {
+        return replies.get(index);
+    }
+
+    public Comment getInReplyTo() {
+        return inReplyTo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Comment by " + author);
+        if (inReplyTo != null) sb.append("in reply to " + inReplyTo.author);
+        if (numReplies() > 0) {
+            for (int i = 0; i < numReplies(); ++i) {
+                sb.append("\n");
+                sb.append(replies[i]);
+            }
+        }
+        return sb;
+    }
+}
